0ee3469caba56cd33ad896165bfe2bb204e89170
Russell <150075502+russellrojas@users.noreply.github.com>
2025-12-07 13:00:45 +0100

    Fix connection string for PostgreSQL

--- php/connexion.php
+++ php/connexion.php
@@ -5,13 +5,11 @@ define("DB_USER", "XXXXXX");
 define("DB_PASS", "your_user");
 define("DB_NAME", "your_date_base");
 
-// Chaîne de connexion PostgreSQL
 $connect = pg_connect("host=".DB_HOST." dbname=".DB_NAME." user=".DB_USER." password=".DB_PASS);
 
 if (!$connect) {
     die("Erreur de connexion à PostgreSQL : " . pg_last_error());
 }
 
-// Optionnel : définir l'encodage en UTF-8
 pg_set_client_encoding($connect, "UTF8");
 ?>
